4591b97f46fa1b32cc142999012a42a774a3f910
Diego <concdiego@gmail.com>
2022-01-28 19:12:21 -0300

    directoryComparer to Map

--- file-copier/src/Main.java
+++ file-copier/src/Main.java
@@ -1,8 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
+    private static String srcDir = "C:\\Users\\Diego\\Nextcloud\\SubidasInstantáneas";
+    private static String checkDir = "E:\\Diego\\Nextcloud";
     public static void main(String[] args) {
-        run();
+        /*run();
         // Sleep for five seconds
         try {
             while(true) {
@@ -10,7 +16,28 @@ public class Main {
             }
         } catch (InterruptedException ignored) {
         }
-        System.out.println("Done.");
+        System.out.println("Done.");*/
+        Compare compare = new Compare();
+		try
+		{
+            File dir1 = new File(srcDir);
+            File dir2 = new File(checkDir);
+			Map<String,File> files = compare.getDiff(dir1,dir2);
+            System.out.println("Done comparing, files only in src:");
+            Iterator<String> i = files.keySet().iterator();
+            while(i.hasNext()){
+                String n = i.next();
+                File f = files.get(n);
+                System.out.println(f.getAbsolutePath());
+            }
+            
+		}
+		catch(IOException ie)
+		{
+			ie.printStackTrace();
+		}
+        //Set<String> ret = dirDiff.compareDir(checkDir);
+        //System.out.println(ret.size());
     }
 
     private static void run(){
@@ -19,8 +46,9 @@ public class Main {
             public void run() {
                 // Repeat forever
                 while (true) {
-                    DirectoryComparer dirDiff = new DirectoryComparer("",".+","*");
-                    Set<String> ret;
+                    /*DirectoryComparer dirDiff = new DirectoryComparer(srcDir,".+","*");
+                    Set<String> ret = dirDiff.compareDir(checkDir);
+                    System.out.println(ret.size());*/
                     System.out.println("I'm a Daemon.");
                     // Sleep for half a second
                     try {
